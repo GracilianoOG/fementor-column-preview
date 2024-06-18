0f0153854d543392e88cbb441de5c5988592396e
Gabriel <gabriel.gracilianomb@gmail.com>
2024-06-18 16:35:45 -0300

    Add basic card template

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import './App.css'
 
 function App() {
   return (
-    <div>Hello world</div>
+    <div>
+      <div className='card'>
+        <h2 className='card__title'>Sedans</h2>
+        <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
+        or on your next road trip.</p>
+        <a href="#">Learn more</a>
+      </div>
+    </div>
   )
 }
 
