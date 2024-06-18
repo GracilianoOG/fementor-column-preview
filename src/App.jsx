a4e7394c2cb763bca20ea78583dcde9a90ccd9c8
Gabriel <gabriel.gracilianomb@gmail.com>
2024-06-18 18:23:25 -0300

    Add img as the card icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import icon from './assets/images/icon-sedans.svg'
 
 function App() {
   return (
     <div>
       <div className='card'>
+        <img src={icon} alt="" />
         <h2 className='card__title'>Sedans</h2>
         <p className='card_description'>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
         or on your next road trip.</p>
