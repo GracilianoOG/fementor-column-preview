0c417da365009609990182610b36ce6f89de8c19
Gabriel <gabriel.gracilianomb@gmail.com>
2024-06-19 13:13:27 -0300

    Change old card with card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import './App.css'
-import icon from './assets/images/icon-sedans.svg'
+import iconSedan from './assets/images/icon-sedans.svg'
+import Card from './Card.jsx'
 
 function App() {
   return (
     <div>
-      <div className='card'>
-        <img className='card__icon' src={icon} alt="" />
-        <h2 className='card__title'>Sedans</h2>
-        <p className='card__description'>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
-        or on your next road trip.</p>
-        <a href="#" className='card__btn'>Learn more</a>
-      </div>
+      <Card 
+        icon={iconSedan} 
+        title="Sedan" 
+        color="rgb(227, 136, 38)"
+        description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
+    or on your next road trip." 
+      />
     </div>
   )
 }
