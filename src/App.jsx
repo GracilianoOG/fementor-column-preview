aafaa2dabfebcfeb8b488e6439829517034ca4f0
Gabriel <gabriel.gracilianomb@gmail.com>
2024-06-19 13:19:22 -0300

    Add third card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import iconSedan from './assets/images/icon-sedans.svg'
 import iconSuv from './assets/images/icon-suvs.svg'
+import iconLuxury from './assets/images/icon-luxury.svg'
 import Card from './Card.jsx'
 
 function App() {
@@ -19,6 +20,13 @@ function App() {
         color="rgb(0, 105, 112)"
         description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation 
   and off-road adventures." 
+      />
+      <Card 
+        icon={iconLuxury} 
+        title="Luxury" 
+        color="rgb(0, 66, 65)"
+        description="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
+  rental and arrive in style." 
       />
     </div>
   )
