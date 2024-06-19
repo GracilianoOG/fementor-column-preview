25d7cf89ba16f9e8ef9e2991063d04effaa04fcf
Gabriel <gabriel.gracilianomb@gmail.com>
2024-06-19 13:17:24 -0300

    Add second card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import iconSedan from './assets/images/icon-sedans.svg'
+import iconSuv from './assets/images/icon-suvs.svg'
 import Card from './Card.jsx'
 
 function App() {
@@ -11,6 +12,13 @@ function App() {
         color="rgb(227, 136, 38)"
         description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
     or on your next road trip." 
+      />
+      <Card 
+        icon={iconSuv} 
+        title="SUVs" 
+        color="rgb(0, 105, 112)"
+        description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation 
+  and off-road adventures." 
       />
     </div>
   )
