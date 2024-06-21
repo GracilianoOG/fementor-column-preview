227c3f09bb4a6231d9cfabfa85fe0bac42d1ac5e
Gabriel <gabriel.gracilianomb@gmail.com>
2024-06-21 15:14:27 -0300

    Add hover state to buttons using JSX

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,31 @@
+import { useState } from 'react'
 import './Card.css'
 
 function Card({ icon, title, description, color }) {
+  const [isHover, setIsHover] = useState(false)
+
+  const btnNormal = {
+    backgroundColor: 'rgb(242, 242, 242)',
+    color: color
+  }
+
+  const btnHover = {
+    backgroundColor: 'transparent',
+    color: 'rgb(242, 242, 242)'
+  }
+
   return (
     <div className='card' style={{backgroundColor: color}}>
       <img className='card__icon' src={icon} alt="" />
       <h2 className='card__title'>{ title }</h2>
       <p className='card__description'>{ description }</p>
-      <a href="#" className='card__btn' style={{color: color}}>Learn more</a>
+      <a 
+        href="#" 
+        className='card__btn' 
+        style={!isHover ? {...btnNormal} : {...btnHover}}
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >Learn more</a>
     </div>
   )
 }
