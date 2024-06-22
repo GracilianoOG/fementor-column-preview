fbac1c896fa13c9150d9e8fd1d975fef68889b80
Gabriel <gabriel.gracilianomb@gmail.com>
2024-06-22 19:09:28 -0300

    Replace inline style with Footer.css

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,8 @@
+import './Footer.css'
+
 function Footer() {
   return (
-    <footer style={{position: "absolute", bottom: "0", width: "100%", textAlign: "center", fontSize: "15px"}}>
+    <footer>
       <p className="attribution">
         <span className="attribution__challenger">Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. </span>
         <span className="attribution__coder">Coded by <a href="#">Gabriel</a>.</span>
