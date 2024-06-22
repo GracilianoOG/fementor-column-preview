e7c23a2de7ba84a35924f97b479cf97a87531a1d
Gabriel <gabriel.gracilianomb@gmail.com>
2024-06-22 19:04:43 -0300

    Add Footer component

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer style={{position: "absolute", bottom: "0", width: "100%", textAlign: "center", fontSize: "15px"}}>
+      <p className="attribution">
+        <span className="attribution__challenger">Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. </span>
+        <span className="attribution__coder">Coded by <a href="#">Gabriel</a>.</span>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
