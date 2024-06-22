776b8a5e63a5e336d8e646a6f92e6149e54286ff
Gabriel <gabriel.gracilianomb@gmail.com>
2024-06-22 18:21:42 -0300

    Add main tag to hold card components

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,31 @@ import CardWrapper from './CardWrapper.jsx'
 
 function App() {
   return (
-    <CardWrapper>
-      <Card 
-        icon={iconSedan} 
-        title="Sedans" 
-        color="rgb(227, 136, 38)"
-        description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
-    or on your next road trip." 
-      />
-      <Card 
-        icon={iconSuv} 
-        title="SUVs" 
-        color="rgb(0, 105, 112)"
-        description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation 
-  and off-road adventures." 
-      />
-      <Card 
-        icon={iconLuxury} 
-        title="Luxury" 
-        color="rgb(0, 66, 65)"
-        description="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
-  rental and arrive in style." 
-      />
-    </CardWrapper>
+    <main>
+      <CardWrapper>
+        <Card 
+          icon={iconSedan} 
+          title="Sedans" 
+          color="rgb(227, 136, 38)"
+          description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
+      or on your next road trip." 
+        />
+        <Card 
+          icon={iconSuv} 
+          title="SUVs" 
+          color="rgb(0, 105, 112)"
+          description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation 
+    and off-road adventures." 
+        />
+        <Card 
+          icon={iconLuxury} 
+          title="Luxury" 
+          color="rgb(0, 66, 65)"
+          description="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
+    rental and arrive in style." 
+        />
+      </CardWrapper>
+    </main>
   )
 }
 
