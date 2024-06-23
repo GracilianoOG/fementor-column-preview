c29ae080542e9604efb74716733e8a52292f3933
Gabriel <gabriel.gracilianomb@gmail.com>
2024-06-23 20:43:52 -0300

    Add author link in the footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,13 @@
 import './Footer.css'
 
 function Footer() {
+  const AUTHOR_LINK = "https://github.com/GracilianoOG"
+  
   return (
     <footer>
       <p className="attribution">
         <span className="attribution__challenger">Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. </span>
-        <span className="attribution__coder">Coded by <a href="#">Gabriel</a>.</span>
+        <span className="attribution__coder">Coded by <a href={AUTHOR_LINK}>Gabriel</a>.</span>
       </p>
     </footer>
   )
