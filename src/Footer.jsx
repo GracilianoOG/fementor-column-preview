003e1f76129e4e1f436d6c7e11b786b4b2952e4a
Gabriel <gabriel.gracilianomb@gmail.com>
2024-06-23 20:57:15 -0300

    Remove target property from link

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <footer>
       <p className="attribution">
-        <span className="attribution__challenger">Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. </span>
+        <span className="attribution__challenger">Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. </span>
         <span className="attribution__coder">Coded by <a href={AUTHOR_LINK}>Gabriel</a>.</span>
       </p>
     </footer>
