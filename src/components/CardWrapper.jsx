d61c18cedb37b9734b3ed37d2d6ee598900c5d7d
Gabriel <gabriel.gracilianomb@gmail.com>
2024-07-04 23:24:47 -0300

    Remove wrapper inner div

--- src/components/CardWrapper.jsx
+++ src/components/CardWrapper.jsx
@@ -3,9 +3,7 @@ import './CardWrapper.css'
 function CardWrapper({ children }) {
   return (
     <div className='card-wrapper'>
-      <div className='card-wrapper__content'>
-        {children}
-      </div>
+      {children}
     </div>
   )
 }
